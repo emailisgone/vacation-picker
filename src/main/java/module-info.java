ea244fdff4e45f2103e487e339af1e67e009f8ef
emailisgone <nikita.exe0@gmail.com>
2023-10-17 21:06:03 +0300

    Removing some deprecated stuff. Now it's over for sure. Cya

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.vu.vacationpicker {
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
     requires java.logging;
-    requires barbecue;
 
 
     opens com.vu.vacationpicker to javafx.fxml;
